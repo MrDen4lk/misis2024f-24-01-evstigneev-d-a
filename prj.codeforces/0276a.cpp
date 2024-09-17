b3cc46ad8615c4001c5d7201513e4df900426d63
MrDen4lk <110861419+MrDen4lk@users.noreply.github.com>
2024-09-17 23:49:38 +0300

    Create 0276a.cpp

---
+++ prj.codeforces/0276a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+int main() {
+    int n = 0;
+    int k = 0;
+    int a = 0;
+    int b = 0;
+    int mx = -1e9;
+    std::cin >> n >> k;
+ 
+    for (int i = 0; i < n; i++) {
+        std::cin >> a >> b;
+        mx = std::max(mx, (b <= k ? a : a - (b - k)));
+    }
+    std::cout << mx;
+ 
+    return 0;
+}
