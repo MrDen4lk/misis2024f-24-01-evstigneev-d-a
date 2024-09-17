8940e6705f173b4b0a12d1924143e4a8023e3abf
MrDen4lk <110861419+MrDen4lk@users.noreply.github.com>
2024-09-17 23:43:02 +0300

    Create 0978a.cpp

---
+++ prj.codeforces/0978a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+void solve() {
+    int number = 0;
+    std::vector<int> statistic_array(1001, false);
+ 
+    std::cin >> number;
+    std::vector<int> nums(number, 0);
+    int final_number = number;
+ 
+    for (auto& item : nums) {
+        std::cin >> item;
+        statistic_array[item]++;
+        if (statistic_array[item] > 1) {
+            final_number--;
+        }
+    }
+ 
+    std::cout << final_number << std::endl;
+    for (const auto& item : nums) {
+        if (statistic_array[item] > 1) {
+            statistic_array[item]--;
+        } else {
+            std::cout << item << ' ';
+        }
+    }
+ }
+ 
+int main() {
+    
+    solve();
+ 
+    return 0;
+}
