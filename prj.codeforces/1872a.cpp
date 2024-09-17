93e61d3d0baad684d38bc4d7c4d553a46eedfea9
MrDen4lk <110861419+MrDen4lk@users.noreply.github.com>
2024-09-17 23:41:51 +0300

    Create 1872a.cpp

---
+++ prj.codeforces/1872a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+void solve() {
+    int first_tank = 0;
+    int second_tank = 0;
+    int cup = 0;
+    std::cin >> first_tank >> second_tank >> cup;
+    std::cout << (first_tank == second_tank ? 0 : (std::abs(second_tank - first_tank) - 1) / (2 * cup) + 1) << std::endl;
+ 
+}
+ 
+int main() {
+    int times = 0;
+    std::cin >> times;
+    
+    while (times--) {
+        solve();
+    }
+ 
+    return 0;
+}
