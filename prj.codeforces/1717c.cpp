3a8d16b1e1cb99d1414fc3587d61ee1d535929e1
MrDen4lk <110861419+MrDen4lk@users.noreply.github.com>
2024-09-24 23:13:35 +0300

    Create 1717c.cpp

---
+++ prj.codeforces/1717c.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+void solve() {
+    int amount = 0;
+    std::cin >> amount;
+    std::vector<int> vec_old(amount, 0);
+    std::vector<int> vec_new(amount, 0);
+    for (int& item : vec_old) {
+        std::cin >> item;
+    }
+    for (int& item : vec_new) {
+        std::cin >> item;
+    }
+
+    for (int i = 0; i < amount; i++) {
+        if (vec_old[i] > vec_new[i] || (vec_new[i] > vec_new[(i + 1) % amount] + 1) && vec_new[i] != vec_old[i]) {
+            std::cout << "no" << std::endl;
+            return;
+        }
+    }
+    std::cout << "yes" << std::endl;
+}
+ 
+int main() {
+    int times = 0;
+    std::cin >> times;
+    
+    while (times--) {
+        solve();
+    }
+
+    return 0;
+}
