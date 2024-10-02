bf32c328fe01603856bbb2de1a32bc563c5ed79c
MrDen4lk <110861419+MrDen4lk@users.noreply.github.com>
2024-10-02 10:23:17 +0300

    Create 338b.cpp

---
+++ prj.codeforces/338b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+void solve() {
+    int n = 0;
+    int m = 0;
+    int now = 1;
+    int house = 0;
+    long long cost = 0ll;
+    std::cin >> n >> m;
+    
+    for (int i = 0; i < m; i++) {
+        std::cin >> house;
+        if (house >= now) {
+            cost += house - now;
+        } else {
+            cost += n - now + house;
+        }
+        now = house;
+    }
+
+    std::cout << cost;
+}
+
+int main() {
+    int times = 1;
+    //std::cin >> times;
+
+    while (times--) {
+        solve();
+    }
+
+    return 0;
+}
