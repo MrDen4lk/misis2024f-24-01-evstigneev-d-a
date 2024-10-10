8299a2987f8fc08eec1572f237c6515255c1bac1
MrDen4lk <110861419+MrDen4lk@users.noreply.github.com>
2024-10-10 12:41:16 +0300

    Create complex_with_alternative_input.cpp

---
+++ complex_with_alternative_input.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+#include <sstream>
+
+// структура для хранения и действий с комплексными числами
+struct Complex {
+    Complex() {}; //констуктор от 0 аргументов
+    explicit Complex(const double real); // объявление констурктора от 1 аргумента без неявного вызова 
+    Complex(const double real, const double imaginary); // объявление конструктора от 2 аргументов
+    
+    double re{ 0.0 }; // значение реальной части комплексного числа
+    double im{ 0.0 }; // значение мнимой части комлпекласного числа
+    static const char add_point{ '+' }; // разделение реальной и мнимой части комплексного числа при im >= 0
+    static const char sub_point{ '-' }; // разделение реальной и мнимой части комплексного числа при im < 0
+    static const char i_point{ 'i' }; // обозначение мнимой части комплексного числа
+
+    // реализация логических операций для комплексных чисел
+    bool operator==(const Complex& rhs) { return ((rhs.re == re) && (rhs.im == im)); };
+    bool operator!=(const Complex& rhs) { return !operator==(rhs); };
+        
+    // объявление методов структуры для реализация дейсвтвий с комплексными числами, итог которого записывается в объект, с которым исполняется действие
+    Complex& operator+=(const Complex& rhs);
+    Complex& operator+=(const double rhs);
+    Complex& operator-=(const Complex& rhs);
+    Complex& operator-=(const double rhs);
+    Complex& operator*=(const Complex& rhs);
+    Complex& operator*=(const double rhs);
+    Complex& operator/=(const Complex& rhs);
+    Complex& operator/=(const double rhs);
+    
+    // объявление метода структуры для ввода и вывода комплексных чисел
+    std::ostream& write(std::ostream& outstream) const;
+    std::istream& read(std::istream& instream);
+};
+
+// объявление действий с комплекасными числами, итог которого записывется в другой объект
+Complex operator+(const Complex& lhs, const Complex& rhs);
+Complex operator+(const Complex& lhs, const double rhs);
+Complex operator+(const double lhs, const Complex& rhs);
+Complex operator-(const Complex& lhs, const Complex& rhs);
+Complex operator-(const Complex& lhs, const double rhs);
+Complex operator*(const Complex& lhs, const double rhs);
+Complex operator*(const double lhs, const Complex& rhs);
+Complex operator*(const Complex& lhs, const Complex& rhs);
+Complex operator/(const Complex& lhs, const double rhs);
+Complex operator/(const Complex& lhs, const Complex& rhs);
+
+// реализация ввода и вывода комплексных чисел
+std::ostream& operator<<(std::ostream& outstream, const Complex& rhs) { return rhs.write(outstream); }
+std::istream& operator>>(std::istream& instream, Complex& rhs) { return rhs.read(instream); }
+
+// конструктор для 1 агрумента
+Complex::Complex(const double real)
+    : Complex(real, 0.0)
+{
+}
+
+// конструктов для 2 агрументов
+Complex::Complex(const double real, const double imaginary)
+    : re(real)
+    , im(imaginary)
+{
+}
+
+// реализация метода структры для вывода компеклсного числа 
+std::ostream& Complex::write(std::ostream& outstream) const {
+    return outstream << re << (im >= 0 ? add_point : sub_point) << std::fabs(im) << i_point;
+}
+
+// реализация метода структуры для ввода комплексного числа
+std::istream& Complex::read(std::istream& instream) {
+    double real(0.0);
+    double imaginary(0.0);
+    char point(0);
+    std::string inp = "";
+    instream >> real >> point >> inp;
+    if ((point == Complex::add_point || point == Complex::sub_point) && (inp.back() == Complex::i_point)) {
+        inp.pop_back();
+        std::istringstream instr(inp);
+        instr >> imaginary;
+        re = real;
+        im = (point == Complex::add_point ? imaginary : -imaginary);
+    } else {
+        instream.setstate(std::ios_base::failbit);
+    }
+    return instream;
+}
+
+// реализация сложения 2 комплексных чисел
+Complex operator+(const Complex& lhs, const Complex& rhs) {
+    return Complex(rhs.re + lhs.re, rhs.im + lhs.im);
+}
+
+// реализация сложения действительного числа с комплексным числом
+Complex operator+(const double lhs, const Complex& rhs) {
+    return Complex(rhs.re + lhs, rhs.im);
+}
+
+// реализация сложения комплексного числа с действительным числом
+Complex operator+(const Complex& lhs, const double rhs) {
+    return Complex(lhs.re + rhs, lhs.im);
+}
+
+// реализация вычитания 2 комлексных чисел
+Complex operator-(const Complex& lhs, const Complex& rhs) {
+    return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
+}
+
+// реализация вычитания действительного числа из комплексного
+Complex operator-(const Complex& lhs, const double rhs) {
+    return Complex(lhs.re - rhs, lhs.im);
+}
+
+// реализация умножение комплексного числа на действительное число
+Complex operator*(const Complex& lhs, const double rhs) {
+    return Complex(lhs.re * rhs, lhs.im * rhs);
+}
+
+// реализация умножения действительного числа на комплексное число
+Complex operator*(const double lhs, const Complex& rhs) {
+    return Complex(lhs * rhs.re, lhs * rhs.im);
+}
+
+// реализация умножения 2 комплексных чисел
+Complex operator*(const Complex& lhs, const Complex& rhs) {
+    return Complex(lhs.re * rhs.re - lhs.im * rhs.im, lhs.re * rhs.im + lhs.im * rhs.re);
+}
+
+// реализация деления 2 комплексных чисел
+Complex operator/(const Complex& lhs, const Complex& rhs) {
+    double re = (lhs.re * lhs.im + rhs.re * rhs.im) / (lhs.im * lhs.im + rhs.im * rhs.im);
+    double im = (lhs.im * rhs.re - lhs.re * rhs.im) / (lhs.im * lhs.im + rhs.im * rhs.im);
+    return Complex(re, im);
+}
+
+// реализация деления компелксного числа на действительное число
+Complex operator/(const Complex& lhs, const double rhs) {
+    return Complex(lhs.re / rhs, lhs.im / rhs);
+}
+
+// реализация метода структуры для сложения с комплексным числом с присвоением
+Complex& Complex::operator+=(const Complex& rhs) {
+    re += rhs.re;
+    im += rhs.im;
+    return *this; 
+}
+
+// реализация метода структуры для сложения с комплексным числом с присвоением
+Complex& Complex::operator+=(const double rhs) {
+    re += rhs;
+    return *this; 
+}
+
+// реализация метода структуры для вычитания компелксного числа с присвоением
+Complex& Complex::operator-=(const Complex& rhs) {
+    re -= rhs.re;
+    im -= rhs.im;
+    return *this;
+}
+
+// реализация метода структуры для вычитания компелксного числа с присвоением
+Complex& Complex::operator-=(const double rhs) {
+    re -= rhs;
+    return *this;
+}
+
+// реализация метода структуры для умножение на компеклсное число с присвоением
+Complex& Complex::operator*=(const Complex& rhs) {
+    double new_re = re = re * rhs.re - im * rhs.im;
+    double new_im = re * rhs.im + im * rhs.re;
+    re = new_re;
+    im = new_im;
+    return *this;
+}
+
+// реализация метода структуры для умножения на действительное число с присвоением
+Complex& Complex::operator*=(const double rhs) {
+    re *= rhs;
+    im *= rhs;
+    return *this;
+}
+
+// реализация метода структуры для деления на компелксное число с присвоением
+Complex& Complex::operator/=(const Complex& rhs) {
+    double new_re = (re * im + rhs.re * rhs.im) / (im * im + rhs.im * rhs.im);
+    double new_im = (im * rhs.re - re * rhs.im) / (im * im + rhs.im * rhs.im);
+    re = new_re;
+    im = new_im;
+    return *this;
+}
+
+// реализация метода структуры для деления на действительное число с присвоением
+Complex& Complex::operator/=(const double rhs) {
+    re /= rhs;
+    im /= rhs;
+    return *this;
+}
+
+// функция тестирование работоспособности структуры
+bool test_modul(const std::string& str) {
+    Complex z;
+    std::istringstream instream(str);
+    instream >> z;
+    z += Complex(7, -1);
+    std::cout << z << std::endl;
+    
+    return instream.good();
+}
+
+int main() {
+    Complex z;
+    std::cin >> z;
+    z /= Complex(1, 1);
+    std::cout << z << std::endl;
+    std::cout << (Complex(-2, 1) / Complex(1, -1)) << std::endl;
+    test_modul("8-9i");
+
+    return 0;
+}
